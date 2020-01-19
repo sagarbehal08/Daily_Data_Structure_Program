186896fbfdc1105127e8eea1f16dbd2957b2b985
sagarbehal08 <vivakbehal@gmail.com>
2020-01-19 19:11:45 +0530

    WholeStringReverse.java first commit

---
+++ WholeStringReverse.java
@@ -0,0 +1,28 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class WholeStringReverse {
+
+	public static void main(String[] args) {
+		Scanner o=new Scanner(System.in);
+		System.out.println("Enter String Words are separated by dots => ");
+		String str=o.next();
+		String str1=reverse(str);
+		System.out.println(str1);
+		}
+	public static String reverse(String str)
+	{
+		String strArr[]=str.split("\\.");
+		String newstr="";
+		
+		for(int i=strArr.length-1;i>0;i--)
+		{  
+			newstr+=strArr[i]+".";
+		}
+		newstr+=strArr[0];
+		
+		return newstr;
+	}
+
+}
